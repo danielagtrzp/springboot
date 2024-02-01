b50b59c7f5774271431495433a683d1ad88db116
Gutierrez Parra <lgutierrezparra@deloitte.com>
2024-02-01 16:45:42 -0600

    first commit

---
+++ src/main/java/com/dan/coursespringboot/CoursespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dan.coursespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursespringbootApplication.class, args);
+	}
+
+}
